d82a1082e25d38970e82cacd403b0a25fbaa1dec
Dreval <somecheesewithyourwine@gmail.com>
2018-08-29 09:16:25 +0100

    Adds route API, Does not work without CORS browser addon

--- src/components/Routeinfo.jsx
+++ src/components/Routeinfo.jsx
@@ -1,11 +1,51 @@
-import React from "react";
+import React, { Component } from "react";
+import API_KEY from "./config/routeApiKey";
+import superagent from "superagent";
 
-const Routeinfo = () => {
-  return (
-    <span>
-      47 Minutes to Work <br /> Traffic is Heavy
-    </span>
-  );
-};
+const agent = superagent.agent();
 
-export default Routeinfo;
+class TravelTimeApi extends Component {
+  state = {
+    travelTime: {},
+    newOb: {},
+    time: "",
+    distance: ""
+  };
+  componentDidMount = async () => {
+    await superagent
+      .get(
+        `https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=${
+          this.props.home
+        }&destinations=${this.props.work}&key=${API_KEY}`
+      )
+
+      .then((error, response) => {
+        if (error) {
+          console.error(error);
+          this.setState({ travelTime: JSON.parse(error.text) });
+          const ob = { ...this.state.travelTime.rows };
+          const ob2 = ob[0];
+          this.setState({ newOb: ob2 });
+
+          this.setState({
+            distance: this.state.newOb.elements[0].distance.text,
+            time: this.state.newOb.elements[0].duration.text
+          });
+        } else {
+          console.log(response);
+        }
+      });
+  };
+
+  render() {
+    return (
+      <div>
+        <p className="travelTime">
+          Your travel time is {this.state.time} to travel {this.state.distance}
+        </p>
+      </div>
+    );
+  }
+}
+
+export default TravelTimeApi;
---
+++ src/components/config/routeApiKey.js
@@ -0,0 +1,3 @@
+const API_KEY = "AIzaSyDDGkLxA6U_nJaXu1H3mPmCI9sDYWu2cXw";
+
+export default API_KEY;
